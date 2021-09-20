cd77b42f31d5981f37ec1e367a30ab001f1559b6
Brian Frank <brianfrank93@gmail.com>
2021-09-20 18:54:05 -0300

    agregue inicio

--- JS/data.js
+++ JS/data.js
@@ -3,5 +3,10 @@ const productos = [
         title: "TiaPato",
         text: "Somos un emprendimiento familiar, ubicados en La Plata,Gran Buenos Aires.Nos dedicamos a la realizacion artesanal y personalizada de tortas tematicas de diseño y mesas dulces, para todo tipo de Eventos.Todos los trabajos que realizamos llevan nuestro toque artístico e inspiracion, plasmando la pasion por lo que hacemos y buscando distinguirlos con los detalles y las terminaciones",
         img: "https://i.ibb.co/Rc8S7Md/32-B1-F474-C0-AA-4694-8-EAE-CF447-C46566-B.jpg"
+    },
+    {
+        title: "Patricia",
+        text:"“Amo lo que hago y dejo un poco de mí en cada proyecto. Me encanta ser parte de tus festejos!”",
+        img: "https://i.ibb.co/qj3dnDp/6cea76ce-2341-4012-a0f6-609bcadeba03.jpg"
     }
 ]
